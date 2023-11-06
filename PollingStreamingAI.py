45ef71ed3f22addf75446190e3b8015b46b0a987
EduCatCode <148319229+EduCatCode@users.noreply.github.com>
2023-11-06 16:31:34 +0800

    Update PollingStreamingAI.py
    Added timestamps are also recorded when writing to Data.

--- PollingStreamingAI.py
+++ PollingStreamingAI.py
@@ -8,7 +8,6 @@ from Automation.BDaq import *
 from Automation.BDaq.WaveformAiCtrl import WaveformAiCtrl
 from Automation.BDaq.BDaqApi import AdxEnumToString, BioFailed
 
-
 def save_data_to_csv(data, filename):
     with open(filename, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
@@ -27,14 +26,12 @@ def AdvPollingStreamingAI(deviceDescription, startChannel, channelCount, section
         # 載入配置文件以初始化設備
         wfAiCtrl.loadProfile = profilePath 
 
-
         # 步驟2：設置必要的參數
         wfAiCtrl.conversion.channelStart = startChannel
         wfAiCtrl.conversion.channelCount = channelCount
         wfAiCtrl.record.sectionCount = sectionCount
         wfAiCtrl.record.sectionLength = sectionLength
         
-    
         # 步驟3：開始操作
         ret = wfAiCtrl.prepare()
         if BioFailed(ret):
@@ -44,31 +41,25 @@ def AdvPollingStreamingAI(deviceDescription, startChannel, channelCount, section
         if BioFailed(ret):
             break
 
-
-
         all_data = []
 
         try:
             # 獲取當前的日期和時間
             now = datetime.now()
-            last_split_time = now
             formatted_date = now.strftime('%Y%m%d%H%M%S') 
             csv_file = open(f"{formatted_date}.csv", "w", newline='')
             csv_writer = csv.writer(csv_file)
 
             # 生成列名並寫入CSV
-            columns = [f'channel{i}' for i in range(channelCount)]
+            columns = ['timestamp'] + [f'channel{i}' for i in range(channelCount)]
             csv_writer.writerow(columns)
-
 
             start_time = datetime.now()
             row_count = 0
 
-
             # 步驟4：設備以輪詢方式採集數據
             print("Polling infinite acquisition is in progress, any key to quit!")
         
- 
             while not kbhit():
                 result = wfAiCtrl.getDataF64(USER_BUFFER_SIZE, -1)
                 ret, returnedCount, data, = result[0], result[1], result[2]
@@ -85,18 +76,17 @@ def AdvPollingStreamingAI(deviceDescription, startChannel, channelCount, section
                         print(f"channel {i + startChannel}: Data not available")
 
                 for i in range(0, returnedCount, channelCount):
-                    row_data = []
+                    row_data = [datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')]  # 加入當前時間戳記
                     for j in range(channelCount):
                         index = i + j
                         if index < len(data):
                             row_data.append(data[index])
                         else:
                             pass
+                            
                     all_data.append(row_data)
                     csv_writer.writerow(row_data)
                     row_count += 1
-
-
 
         except KeyboardInterrupt:
             # 步驟6：如果正在運作則停止操作
@@ -106,25 +96,20 @@ def AdvPollingStreamingAI(deviceDescription, startChannel, channelCount, section
             sampling_rate = row_count / elapsed_time
             print(f"Elapsed Time: {elapsed_time} seconds")
             print(f"Sampling Rate: {sampling_rate} rows per second")
-
             rows_per_file = int(split_sec * sampling_rate)  # 計算每個文件的行數
             formatted_date = start_time.strftime('%Y%m%d%H%M%S')
             for i in range(0, len(all_data), rows_per_file):
                 chunk = all_data[i:i+rows_per_file]
                 save_data_to_csv(chunk, f'{formatted_date}({i // rows_per_file + 1}).csv')
 
-
     # 步驟 7: 關閉設備，釋放所有分配的資源
     wfAiCtrl.dispose()
-
 
         # 如果執行中出現問題，將錯誤代碼列印在螢幕上以便跟踪
     if BioFailed(ret):
         enumStr = AdxEnumToString("ErrorCode", ret.value, 256)
         print("Some error occurred. And the last error code is %#x. [%s]" % (ret.value, enumStr))
-
     return 0
-
 
 # 在主函數或程式的適當位置
 if __name__ == '__main__':
@@ -146,7 +131,6 @@ if __name__ == '__main__':
 
     print ('Please set split_sec parameters:')
     split_sec = int(input('split_sec(Ex: 30):\n'))
-
 
     AdvPollingStreamingAI(deviceDescription, startChannel, channelCount, sectionLength, sectionCount, split_sec)
 
